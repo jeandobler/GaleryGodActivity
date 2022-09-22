34fea27d58ec79d8971083d0d570968c62830139
Jecadev <jedobler@gmail.com>
2022-09-21 23:31:25 -0300

    First commit

--- app/src/main/java/com/dobler/galerygodactivity/MainActivity.kt
+++ app/src/main/java/com/dobler/galerygodactivity/MainActivity.kt
@@ -1,83 +1,206 @@
 package com.dobler.galerygodactivity
 
-import android.net.Uri
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
 import android.os.Bundle
-import android.os.Environment
+import android.telecom.Call
+import android.text.TextUtils
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.View.OnKeyListener
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import java.io.File
-import java.util.Locale
+import com.dobler.galerygodactivity.WeatherManager.FindResult
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.Response
+import javax.security.auth.callback.Callback
 import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
-    private var mAdapter: RecyclerView.Adapter<*>? = null
-    private var mLayoutManager: RecyclerView.LayoutManager? = null
-    var medias: List<Media>? = null
+    private var mSharedPref: SharedPreferences? = null
+    private var mAdapter: FindItemAdapter? = null
+    private val cities: ArrayList<WeatherManager.City> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mRecyclerView = findViewById<RecyclerView>(R.id.recycler_medias)
-        medias = getFilePaths("Download")
-        mRecyclerView.setHasFixedSize(true)
-        mLayoutManager = GridLayoutManager(this, 2)
-        mRecyclerView.layoutManager = mLayoutManager
-        mAdapter = MediasAdapter(medias)
-        mRecyclerView.adapter = mAdapter
-    }
+        mSharedPref = getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+        mAdapter = FindItemAdapter(this, cities)
+        newList.adapter = mAdapter
 
-    class MediasAdapter(private val mMedias: List<Media>?) :
-        RecyclerView.Adapter<MediasAdapter.MediaViewHolder>() {
-        override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): MediaViewHolder {
-            val view = LayoutInflater.from(
-                viewGroup.context
-            ).inflate(R.layout.item_view_gallery, viewGroup, false)
-            return MediaViewHolder(view)
-        }
 
-        override fun onBindViewHolder(mediaViewHolder: MediaViewHolder, i: Int) {
-            val media = mMedias!![i]
-            mediaViewHolder.imageViewThumbnail.setImageURI(Uri.fromFile(File(media.thumbnailLocalPath)))
-        }
+        editText.setOnKeyListener { v, keyCode, event ->
 
-        override fun getItemCount(): Int {
-            return mMedias?.size ?: 0
-        }
-
-        class MediaViewHolder(viewRoot: View) : RecyclerView.ViewHolder(viewRoot) {
-            var imageViewThumbnail: ImageView = itemView.findViewById(R.id.image_media_thumbnail)
-
+            if (event.getAction() === KeyEvent.ACTION_DOWN &&
+                keyCode == KeyEvent.KEYCODE_ENTER
+            ) {
+                searchByName()
+            }
+            false
         }
     }
 
-    inner class Media(val thumbnailLocalPath: String)
 
-    fun getFilePaths(dir: String): List<Media> {
-        val medias: MutableList<Media> = ArrayList()
-        val directory =
-            File(Environment.getExternalStorageDirectory().toString() + File.separator.toString() + dir)
-        if (directory.isDirectory) {
-            for (file in directory.listFiles()) {
-                val filePath: String = file.absolutePath
-                if (isSupportedFile(filePath)) {
-                    medias.add(Media(filePath))
-                }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id: Int = item.getItemId()
+        if (id == R.id.menu_celcius) {
+            updateUnitIfNecessary("metric")
+            return true
+        } else if (id == R.id.menu_fahrenheit) {
+            updateUnitIfNecessary("imperial")
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateUnitIfNecessary(newUnits: String) {
+        val currentUnits = temperatureUnit
+        if (currentUnits != newUnits) {
+            temperatureUnit = newUnits
+            searchByName()
+        }
+    }
+
+    fun onSearchClick(view: View?) {
+        searchByName()
+    }
+
+    private fun onStartLoading() {
+        newList.setVisibility(View.GONE)
+        progressBar!!.visibility = View.VISIBLE
+        textView!!.visibility = View.GONE
+        val view: View? = this.currentFocus
+        if (view != null) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
+        }
+    }
+
+    private fun onFinishLoading(result: FindResult?) {
+        progressBar!!.visibility = View.GONE
+        cities.clear()
+        result?.list?.let {
+            if (it.size > 0) {
+                cities.addAll(it)
+                newList.setVisibility(View.VISIBLE)
+                mAdapter!!.notifyDataSetChanged()
+            } else {
+                textView!!.text = "No results."
             }
         }
-        return medias
     }
 
-    private fun isSupportedFile(filePath: String): Boolean {
-        val ext = filePath.substring(filePath.lastIndexOf(".") + 1, filePath.length)
-        return FILE_EXTENSIONS.contains(ext.lowercase(Locale.getDefault()))
+    private fun onFinishLoadingWithError() {
+        progressBar!!.visibility = View.GONE
+        newList.setVisibility(View.GONE)
+        textView!!.text = "Error"
+    }
+
+    val isDeviceConnected: Boolean
+        get() {
+            val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val netInfo = cm.activeNetworkInfo
+            return netInfo != null && netInfo.isConnected
+        }
+
+    private fun searchByName() {
+        if (!isDeviceConnected) {
+            Toast.makeText(this, "No connection!", Toast.LENGTH_LONG).show()
+            return
+        }
+        val search = editText!!.text.toString()
+        if (TextUtils.isEmpty(search)) {
+            return
+        }
+        onStartLoading()
+        val wService: WeatherManager.WeatherService = WeatherManager.service
+        val units = temperatureUnit
+        val findCall = wService.find(search, units, WeatherManager.API_KEY)
+        findCall.enqueue(object : retrofit2.Callback<FindResult?> {
+            override fun onResponse(
+                call: retrofit2.Call<FindResult?>,
+                response: retrofit2.Response<FindResult?>
+            ) {
+                onFinishLoading(response.body())
+            }
+
+            override fun onFailure(call: retrofit2.Call<FindResult?>, t: Throwable) {
+                onFinishLoadingWithError()
+            }
+        })
+    }
+
+    var temperatureUnit: String?
+        get() = mSharedPref!!.getString(TEMPERATURE_UNIT_KEY, "metric")
+        set(value) {
+            val editor = mSharedPref!!.edit()
+            editor.putString(TEMPERATURE_UNIT_KEY, value)
+            editor.apply()
+        }
+
+    inner class FindItemAdapter(context: Context, cities: ArrayList<WeatherManager.City>) :
+        ArrayAdapter<WeatherManager.City>(context, 0, cities) {
+
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var convertView = convertView
+            val city = getItem(position)
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context)
+                    .inflate(R.layout.city_list_item, parent, false)
+
+                val cityName: TextView = convertView.findViewById(R.id.cityNameTxt)
+                cityName.setText(city?.title)
+                val description: TextView = convertView.findViewById(R.id.descriptionTxt)
+                description.setText(city?.description)
+                val metric: TextView = convertView.findViewById(R.id.metricTxt)
+                val units = temperatureUnit
+                if ("metric" == units) {
+                    metric.text = "ºC"
+                } else {
+                    metric.text = "ºF"
+                }
+                val temp: TextView = convertView.findViewById(R.id.tempTxt)
+                temp.setText(city?.temperature)
+                val wind: TextView = convertView.findViewById(R.id.windTxt)
+                if ("metric" == units) {
+                    wind.setText(city?.wind.toString() + " m/s")
+                } else {
+                    wind.setText(city?.wind.toString() + " m/h")
+                }
+                val clouds: TextView = convertView.findViewById(R.id.cloudsTxt)
+                clouds.setText(city?.getClouds())
+                val pressure: TextView = convertView.findViewById(R.id.pressureTxt)
+                pressure.setText(city?.pressure)
+                val resId = context.resources.getIdentifier(
+                    "w_" + city?.weather?.get(0)?.icon,
+                    "drawable",
+                    context.packageName
+                )
+                val icon: ImageView = convertView.findViewById(R.id.weatherIcon)
+                icon.setImageResource(resId)
+                return convertView
+
+            }
+            return parent
+        }
     }
 
     companion object {
-        val FILE_EXTENSIONS: List<String> = listOf("jpg", "jpeg", "png")
+        private const val PREFERENCE_NAME = "com.rperazzo.weatherapp.shared"
+        private const val TEMPERATURE_UNIT_KEY = "TEMPERATURE_UNIT_KEY"
     }
 }
